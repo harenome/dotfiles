15e584b5eae238f1851a4ed3bbbd25db029a60c9
Harenome Ranaivoarivony Razanajato <razanajato@etu.unistra.fr>
2013-11-16 10:51:54 +0100

    fish completions for my script 'dmux'

--- fish/completions/dmux.fish
+++ fish/completions/dmux.fish
@@ -1,5 +1,5 @@
-complete -c dmux -A -f
-complete -c dmux -f -n "tmux ls > /dev/null" -a "(tmux ls -F \"#{session_name}\")" -d 'tmux session'
+complete -c dmux -A -x
+complete -c dmux -f -n "tmux ls 2> /dev/null 1>&2" -a "(tmux ls -F \"#{session_name}\")" -d 'tmux session'
 complete -c dmux -f -s k -l kill-both -d 'Kill both sessions on exit'
 complete -c dmux -f -s f -l force-kill-both -d 'Force kill'
 complete -c dmux -f -s h -l help -d 'Help'
