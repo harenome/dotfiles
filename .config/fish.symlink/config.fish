ee6e4b9b21e9eb438d1128ce16b5d455e56c20dd
Harenome Ranaivoarivony Razanajato <razanajato@etu.unistra.fr>
2015-05-27 10:11:16 +0200

    add xdg variables

--- .config/fish.symlink/config.fish
+++ .config/fish.symlink/config.fish
@@ -99,6 +99,16 @@ if [ -d $HOME_OPT ]
 end
 
 ################################################################################
+# XDG variables
+################################################################################
+
+set -x XDG_DATA_HOME "$HOME/.local/share"
+set -x XDG_CONFIG_HOME "$HOME/.config"
+set -x XDG_DATA_DIRS "/usr/local/share/:/usr/share/"
+set -x XDG_CONFIG_DIRS "/etc/xdg"
+set -x XDG_CACHE_HOME "$HOME/.cache"
+
+################################################################################
 # Local configurations.
 ################################################################################
 
