437fe77851e420d0d7d7cae9052b0f617ae38a99
Harenome Ranaivoarivony-Razanajato <ranaivoarivony-razanajato@hareno.me>
2017-04-10 10:01:59 +0200

    .config/dotfiles: remove redundant examples

--- .config/dotfiles/fish/gnome-keyring.fish
+++
@@ -1,6 +0,0 @@
-################################################################################
-# gnome-keyring-dameon environment variables
-################################################################################
-
-set -xU SSH_AUTH_SOCK (gnome-keyring-daemon -s | grep SSH_AUTH_SOCK | cut -d '=' -f 2)
-set -xU GPG_AGENT_INFO (gnome-keyring-daemon -s | grep GPG_AGENT_INFO | cut -d '=' -f 2)
